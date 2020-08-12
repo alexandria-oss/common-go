8851b0c6c1f795a62f4e1e9cd827c6f1bcaf569f
A Ruiz <luis.alonso.16@hotmail.com>
2020-08-11 23:59:59 -0600

    Update exception testing

--- test/errors_test.go
+++ test/errors_test.go
@@ -9,5 +9,30 @@ func TestNewCustomError(t *testing.T) {
 	custom := exception.NewCustomError(exception.AlreadyExists, "foo")
 	t.Log(custom)
 
-	t.Log(exception.GetDescription(custom))
+	desc := exception.GetDescription(custom)
+	t.Log(desc)
+
+	if desc != "foo already exists" {
+		t.Fatal("invalid error description, expected 'foo already exists'")
+	}
+
+	custom = exception.NewCustomError(exception.NotFound, "bar")
+	t.Log(custom)
+
+	desc = exception.GetDescription(custom)
+	t.Log(desc)
+
+	if desc != "bar not found" {
+		t.Fatal("invalid error description, expected 'bar not found'")
+	}
+
+	custom = exception.NewCustomError(exception.FieldRange, "foo", "1", "255")
+	t.Log(custom)
+
+	desc = exception.GetDescription(custom)
+	t.Log(desc)
+
+	if desc != "field foo is out of range [1, 255)" {
+		t.Fatal("invalid error description, expected 'field foo is out of range [1, 255)'")
+	}
 }
